82dc6d266ad1258d6294ba0c21accd0904e888c5
Jeffrey Walton <noloader@gmail.com>
2017-10-12 16:40:25 -0400

    Add script to build Wget This may be needed for old Wget's, like 1.11 on CentOS 5. Wget 1.11 lacks SNI support, so it can't download from some sites like cURL's haxx.se because of shared hosting and a super-cert

---
+++ build-wget.sh
@@ -0,0 +1,402 @@
+#!/usr/bin/env bash
+
+# See fixup for INSTALL_LIBDIR below
+INSTALL_PREFIX=/usr/local
+INSTALL_LIBDIR="$INSTALL_PREFIX/lib64"
+
+OPENSSL_TAR=openssl-1.0.2l.tar.gz
+OPENSSL_DIR=openssl-1.0.2l
+#OPENSSL_TAR=openssl-1.1.0e.tar.gz
+#OPENSSL_DIR=openssl-1.1.0e
+
+ZLIB_TAR=zlib-1.2.11.tar.gz
+ZLIB_DIR=zlib-1.2.11
+
+UNISTR_TAR=libunistring-0.9.7.tar.gz
+UNISTR_DIR=libunistring-0.9.7
+
+ICONV_TAR=libiconv-1.15.tar.gz
+ICONV_DIR=libiconv-1.15
+
+WGET_TAR=wget-1.19.1.tar.gz
+WGET_DIR=wget-1.19.1
+
+###############################################################################
+
+# Autotools on Solaris has an implied requirement for GNU gear. Things fall apart without it.
+# Also see https://blogs.oracle.com/partnertech/entry/preparing_for_the_upcoming_removal.
+if [[ -d "/usr/gnu/bin" ]]; then
+    if [[ ! ("$PATH" == *"/usr/gnu/bin"*) ]]; then
+        echo
+        echo "Adding /usr/gnu/bin to PATH for Solaris"
+        PATH="/usr/gnu/bin:$PATH"
+    fi
+elif [[ -d "/usr/swf/bin" ]]; then
+    if [[ ! ("$PATH" == *"/usr/sfw/bin"*) ]]; then
+        echo
+        echo "Adding /usr/sfw/bin to PATH for Solaris"
+        PATH="/usr/sfw/bin:$PATH"
+    fi
+elif [[ -d "/usr/ucb/bin" ]]; then
+    if [[ ! ("$PATH" == *"/usr/ucb/bin"*) ]]; then
+        echo
+        echo "Adding /usr/ucb/bin to PATH for Solaris"
+        PATH="/usr/ucb/bin:$PATH"
+    fi
+fi
+
+###############################################################################
+
+#if [[ -z `which autoreconf` ]]; then
+    #echo "Some packages require autoreconf. Please install autoconf or automake."
+    #[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+#fi
+
+###############################################################################
+
+echo
+echo "If you enter a sudo password, then it will be used for installation."
+echo "If you don't enter a password, then ensure INSTALL_PREFIX is writable."
+echo "To avoid sudo and the password, just press ENTER and they won't be used."
+read -s -p "Please enter password for sudo: " SUDO_PASSWWORD
+echo
+
+###############################################################################
+
+THIS_SYSTEM=$(uname -s 2>&1)
+IS_DARWIN=$(echo -n "$THIS_SYSTEM" | grep -i -c darwin)
+IS_LINUX=$(echo -n "$THIS_SYSTEM" | grep -i -c linux)
+IS_CYGWIN=$(echo -n "$THIS_SYSTEM" | grep -i -c cygwin)
+IS_MINGW=$(echo -n "$THIS_SYSTEM" | grep -i -c mingw)
+IS_OPENBSD=$(echo -n "$THIS_SYSTEM" | grep -i -c openbsd)
+IS_DRAGONFLY=$(echo -n "$THIS_SYSTEM" | grep -i -c dragonfly)
+IS_FREEBSD=$(echo -n "$THIS_SYSTEM" | grep -i -c freebsd)
+IS_NETBSD=$(echo -n "$THIS_SYSTEM" | grep -i -c netbsd)
+IS_SOLARIS=$(echo -n "$THIS_SYSTEM" | grep -i -c sunos)
+
+if [[ ("$IS_FREEBSD" -eq "1" || "$IS_OPENBSD" -eq "1" || "$IS_NETBSD" -eq "1" || "$IS_DRAGONFLY" -eq "1" || "$IS_SOLARIS" -eq "1") ]]; then
+    if [[ !(-z `which gmake 2>/dev/null | grep -v 'no gmake'`) ]]; then
+        MAKE=gmake
+    else
+        MAKE=make
+    fi
+else
+    MAKE=make
+fi
+
+# Try to determine 32 vs 64-bit, /usr/local/lib, /usr/local/lib32 and /usr/local/lib64
+# The Autoconf programs misdetect Solaris as x86 even though its x64. OpenBSD has
+# getconf, but it does not have LONG_BIT.
+IS_64BIT=$(getconf LONG_BIT 2>&1 | grep -i -c 64)
+if [[ "$IS_64BIT" -eq "0" ]]; then
+    IS_64BIT=$(file /bin/ls 2>&1 | grep -i -c '64-bit')
+fi
+
+if [[ "$IS_SOLARIS" -eq "1" ]]; then
+    SH_KBITS=64
+    SH_MARCH=-m64
+    INSTALL_LIBDIR="$INSTALL_PREFIX/lib64"
+    INSTALL_LIBDIR_DIR="lib64"
+elif [[ "$IS_64BIT" -eq "1" ]]; then
+    if [[ (-d /usr/lib) && (-d /usr/lib32) ]]; then
+        SH_KBITS=64
+        SH_MARCH=-m64
+        INSTALL_LIBDIR="$INSTALL_PREFIX/lib"
+        INSTALL_LIBDIR_DIR="lib"
+    elif [[ (-d /usr/lib) && (-d /usr/lib64) ]]; then
+        SH_KBITS=64
+        SH_MARCH=-m64
+        INSTALL_LIBDIR="$INSTALL_PREFIX/lib64"
+        INSTALL_LIBDIR_DIR="lib64"
+    else
+        SH_KBITS=64
+        SH_MARCH=-m64
+        INSTALL_LIBDIR="$INSTALL_PREFIX/lib"
+        INSTALL_LIBDIR_DIR="lib"
+    fi
+else
+    SH_KBITS=32
+    SH_MARCH=-m32
+    INSTALL_LIBDIR="$INSTALL_PREFIX/lib"
+    INSTALL_LIBDIR_DIR="lib"
+fi
+
+if [[ -z "$CC" ]]; then CC=`which cc`; fi
+
+MARCH_ERROR=`$CC $SH_MARCH -x c -c -o /dev/null - </dev/null 2>&1 | grep -i -c error`
+if [[ "$MARCH_ERROR" -ne "0" ]]; then
+	SH_MARCH=
+fi
+
+echo
+echo "********** libdir **********"
+echo
+echo "Using libdir $INSTALL_LIBDIR"
+
+###############################################################################
+
+echo
+echo "********** zLib **********"
+echo
+
+wget "http://www.zlib.net/$ZLIB_TAR" -O "$ZLIB_TAR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to download zLib"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+rm -rf "$ZLIB_DIR" &>/dev/null
+tar -xzf "$ZLIB_TAR"
+cd "$ZLIB_DIR"
+
+SH_LDLIBS=("-ldl -lpthread")
+SH_LDFLAGS=("$SH_MARCH" "-Wl,-rpath,$INSTALL_LIBDIR" "-L$INSTALL_LIBDIR")
+
+CPPFLAGS="-I$INSTALL_PREFIX/include -DNDEBUG" CFLAGS="$SH_MARCH" CXXFLAGS="$SH_MARCH" \
+    LDFLAGS="${SH_LDFLAGS[@]}" LIBS="${SH_LDLIBS[@]}" \
+    ./configure --enable-shared --prefix="$INSTALL_PREFIX" --libdir="$INSTALL_LIBDIR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to configure zLib"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=(-j 4)
+"$MAKE" "${MAKE_FLAGS[@]}"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to build zLib"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=(install)
+if [[ ! (-z "$SUDO_PASSWWORD") ]]; then
+    echo "$SUDO_PASSWWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd ..
+
+###############################################################################
+
+echo
+echo "********** Unistring **********"
+echo
+
+# https://savannah.gnu.org/bugs/?func=detailitem&item_id=26786
+wget "https://ftp.gnu.org/gnu/libunistring/$UNISTR_TAR" --no-check-certificate -O "$UNISTR_TAR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to download IDN"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+rm -rf "$UNISTR_DIR" &>/dev/null
+tar -xzf "$UNISTR_TAR"
+cd "$UNISTR_DIR"
+
+SH_LDLIBS=("-ldl -lpthread")
+SH_LDFLAGS=("$SH_MARCH" "-Wl,-rpath,$INSTALL_LIBDIR" "-L$INSTALL_LIBDIR")
+
+CPPFLAGS="-I$INSTALL_PREFIX/include -DNDEBUG" CFLAGS="$SH_MARCH" CXXFLAGS="$SH_MARCH" \
+    LDFLAGS="${SH_LDFLAGS[@]}" LIBS="${SH_LDLIBS[@]}" \
+    ./configure --enable-shared --prefix="$INSTALL_PREFIX" --libdir="$INSTALL_LIBDIR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to configure IDN"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=(-j 4)
+"$MAKE" "${MAKE_FLAGS[@]}"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to build IDN"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=(install)
+if [[ ! (-z "$SUDO_PASSWWORD") ]]; then
+    echo "$SUDO_PASSWWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd ..
+
+###############################################################################
+
+echo
+echo "********** iConvert **********"
+echo
+
+# https://savannah.gnu.org/bugs/?func=detailitem&item_id=26786
+wget "https://ftp.gnu.org/pub/gnu/libiconv/$ICONV_TAR" --no-check-certificate -O "$ICONV_TAR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to download iConvert"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+rm -rf "$ICONV_DIR" &>/dev/null
+tar -xzf "$ICONV_TAR"
+cd "$ICONV_DIR"
+
+SH_LDFLAGS=("$SH_MARCH" "-Wl,-rpath,$INSTALL_LIBDIR" "-L$INSTALL_LIBDIR")
+SH_LDLIBS=("-ldl" "-lpthread")
+
+CPPFLAGS="-I$INSTALL_PREFIX/include -DNDEBUG" CFLAGS="$SH_MARCH" CXXFLAGS="$SH_MARCH" \
+    LDFLAGS="${SH_LDFLAGS[@]}" LIBS="${SH_LDLIBS[@]}" \
+    ./configure --enable-shared --prefix="$INSTALL_PREFIX" --libdir="$INSTALL_LIBDIR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to configure iConvert"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=(-j 4)
+"$MAKE" "${MAKE_FLAGS[@]}"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to build iConvert"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=(install)
+if [[ ! (-z "$SUDO_PASSWWORD") ]]; then
+    echo "$SUDO_PASSWWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd ..
+
+###############################################################################
+
+echo
+echo "********** OpenSSL **********"
+echo
+
+# wget on Ubuntu 16 cannot validate against Let's Encrypt certificate
+wget "https://www.openssl.org/source/$OPENSSL_TAR" --no-check-certificate -O "$OPENSSL_TAR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to download OpenSSL"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+rm -rf "$OPENSSL_DIR" &>/dev/null
+tar -xzf "$OPENSSL_TAR"
+cd "$OPENSSL_DIR"
+
+# OpenSSL and enable-ec_nistp_64_gcc_128 option
+IS_X86_64=$(uname -m 2>&1 | egrep -i -c "(amd64|x86_64)")
+if [[ "$SH_KBITS" -eq "32" ]]; then IS_X86_64=0; fi
+
+CONFIG=./config
+CONFIG_FLAGS=("no-ssl2" "no-ssl3" "no-comp" "shared" "-DNDEBUG" "-Wl,-rpath,$INSTALL_LIBDIR"
+        "--prefix=$INSTALL_PREFIX" "--openssldir=$INSTALL_PREFIX" "--libdir=$INSTALL_LIBDIR_DIR")
+if [[ "$IS_X86_64" -eq "1" ]]; then
+    CONFIG_FLAGS+=("enable-ec_nistp_64_gcc_128")
+fi
+
+KERNEL_BITS="$SH_KBITS" "$CONFIG" "${CONFIG_FLAGS[@]}"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to configure OpenSSL"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=(-j 4 depend)
+"$MAKE" "${MAKE_FLAGS[@]}"
+
+MAKE_FLAGS=(-j 4)
+"$MAKE" "${MAKE_FLAGS[@]}"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to build OpenSSL"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=(install_sw)
+if [[ ! (-z "$SUDO_PASSWWORD") ]]; then
+    echo "$SUDO_PASSWWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd ..
+
+###############################################################################
+
+echo
+echo "********** Wget **********"
+echo
+
+# https://savannah.gnu.org/bugs/?func=detailitem&item_id=26786
+wget "ftp://mirrors.ocf.berkeley.edu/gnu/wget/$WGET_TAR" -O "$WGET_TAR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to download SSH"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+rm -rf "$WGET_DIR" &>/dev/null
+tar -xzf "$WGET_TAR"
+cd "$WGET_DIR"
+
+SH_LDFLAGS=("$SH_MARCH" "-Wl,-rpath,$INSTALL_LIBDIR" "-L$INSTALL_LIBDIR")
+SH_LDLIBS=("-lssl" "-lcrypto" "-ldl" "-lpthread")
+
+CPPFLAGS="-I$INSTALL_PREFIX/include -DNDEBUG" CFLAGS="$SH_MARCH" CXXFLAGS="$SH_MARCH" \
+    LDFLAGS="${SH_LDFLAGS[@]}" LIBS="${SH_LDLIBS[@]}" \
+    ./configure --prefix="$INSTALL_PREFIX" --libdir="$INSTALL_LIBDIR" \
+    --with-ssl=openssl --with-libssl-prefix="$INSTALL_PREFIX" \
+	--with-libiconv-prefix="$INSTALL_PREFIX" --with-libunistring-prefix="$INSTALL_PREFIX"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to configure SSH"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=(-j 4 all)
+"$MAKE" "${MAKE_FLAGS[@]}"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to build SSH"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=(install)
+if [[ ! (-z "$SUDO_PASSWWORD") ]]; then
+    echo "$SUDO_PASSWWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd ..
+
+###############################################################################
+
+echo
+echo "********** Cleanup **********"
+echo
+
+# Set to false to retain artifacts
+if true; then
+
+    ARTIFACTS=("$ZLIB_TAR" "$ZLIB_DIR" "OPENSSL_TAR" "OPENSSL_DIR" "UNISTR_TAR" "UNISTR_DIR" "ICONV_TAR" "ICONV_DIR" "$WGET_TAR" "$WGET_DIR")
+
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-ssh.sh 2>&1 | tee build-ssh.log
+    if [[ -e build-ssh.log ]]; then
+        rm build-ssh.log
+    fi
+fi
+
+[[ "$0" = "$BASH_SOURCE" ]] && exit 0 || return 0
