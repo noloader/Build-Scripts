bd7e38f7dc548f71d8f3ff0ec86a0141e6b92650
Jeffrey Walton <noloader@gmail.com>
2017-10-23 17:02:44 -0400

    Added build-guile.sh build-curl.sh build-iconv.sh build-nettle.sh build-tasn1.sh scripts

---
+++ build-curl.sh
@@ -0,0 +1,327 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds cURL from sources.
+
+# See fixup for INSTALL_LIBDIR below
+INSTALL_PREFIX=/usr/local
+INSTALL_LIBDIR="$INSTALL_PREFIX/lib64"
+
+CURL_TAR=curl-7.56.1.tar.gz
+CURL_DIR=curl-7.56.1
+
+# Avoid shellcheck.net warning
+CURR_DIR="$PWD"
+
+# Sets the number of make jobs if not set in environment
+: "${MAKE_JOBS:=4}"
+
+###############################################################################
+
+# Autotools on Solaris has an implied requirement for GNU gear. Things fall apart without it.
+# Also see https://blogs.oracle.com/partnertech/entry/preparing_for_the_upcoming_removal.
+if [[ -d "/usr/gnu/bin" ]]; then
+    if [[ ! ("$PATH" == *"/usr/gnu/bin"*) ]]; then
+        echo
+        echo "Adding /usr/gnu/bin to PATH for Solaris"
+        PATH="/usr/gnu/bin:$PATH"
+    fi
+elif [[ -d "/usr/swf/bin" ]]; then
+    if [[ ! ("$PATH" == *"/usr/sfw/bin"*) ]]; then
+        echo
+        echo "Adding /usr/sfw/bin to PATH for Solaris"
+        PATH="/usr/sfw/bin:$PATH"
+    fi
+elif [[ -d "/usr/ucb/bin" ]]; then
+    if [[ ! ("$PATH" == *"/usr/ucb/bin"*) ]]; then
+        echo
+        echo "Adding /usr/ucb/bin to PATH for Solaris"
+        PATH="/usr/ucb/bin:$PATH"
+    fi
+fi
+
+###############################################################################
+
+if [[ -z $(command -v gzip 2>/dev/null) ]]; then
+    echo "Some packages gzip. Please install gzip."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+IS_DARWIN=$(uname -s | grep -i -c darwin)
+if [[ ("$IS_DARWIN" -eq "0") ]] && [[ -z $(command -v libtoolize 2>/dev/null) ]]; then
+    echo "Some packages require libtool. Please install libtool or libtool-bin."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+if [[ -z $(command -v autoreconf 2>/dev/null) ]]; then
+    echo "Some packages require autoreconf. Please install autoconf or automake."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+if [[ ! -f "$HOME/.cacert/lets-encrypt-root-x3.pem" ]]; then
+    echo "cURL requires several CA roots. Please run build-cacert.sh."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+LETS_ENCRYPT_ROOT="$HOME/.cacert/lets-encrypt-root-x3.pem"
+CURL_CA_ZOO="$HOME/.cacert/cacert.pem"
+
+###############################################################################
+
+THIS_SYSTEM=$(uname -s 2>&1)
+IS_DARWIN=$(echo -n "$THIS_SYSTEM" | grep -i -c darwin)
+IS_SOLARIS=$(echo -n "$THIS_SYSTEM" | grep -i -c sunos)
+
+# The BSDs and Solaris should have GMake installed if its needed
+if [[ $(command -v gmake 2>/dev/null) ]]; then
+    MAKE="gmake"
+else
+    MAKE="make"
+fi
+
+# Try to determine 32 vs 64-bit, /usr/local/lib, /usr/local/lib32 and /usr/local/lib64
+# The Autoconf programs misdetect Solaris as x86 even though its x64. OpenBSD has
+# getconf, but it does not have LONG_BIT.
+IS_64BIT=$(getconf LONG_BIT 2>&1 | grep -i -c 64)
+if [[ "$IS_64BIT" -eq "0" ]]; then
+    IS_64BIT=$(file /bin/ls 2>&1 | grep -i -c '64-bit')
+fi
+
+if [[ "$IS_SOLARIS" -ne "0" ]]; then
+    SH_MARCH="-m64"
+    INSTALL_LIBDIR="$INSTALL_PREFIX/lib64"
+elif [[ "$IS_64BIT" -ne "0" ]]; then
+    if [[ (-d /usr/lib) && (-d /usr/lib32) ]]; then
+        SH_MARCH="-m64"
+        INSTALL_LIBDIR="$INSTALL_PREFIX/lib"
+    elif [[ (-d /usr/lib) && (-d /usr/lib64) ]]; then
+        SH_MARCH="-m64"
+        INSTALL_LIBDIR="$INSTALL_PREFIX/lib64"
+    else
+        SH_MARCH="-m64"
+        INSTALL_LIBDIR="$INSTALL_PREFIX/lib"
+    fi
+else
+    SH_MARCH="-m32"
+    INSTALL_LIBDIR="$INSTALL_PREFIX/lib"
+fi
+
+if [[ (-z "$CC" && $(command -v cc 2>/dev/null) ) ]]; then CC=$(command -v cc); fi
+if [[ (-z "$CXX" && $(command -v CC 2>/dev/null) ) ]]; then CXX=$(command -v CC); fi
+
+MARCH_ERROR=$($CC $SH_MARCH -x c -c -o /dev/null - </dev/null 2>&1 | grep -i -c error)
+if [[ "$MARCH_ERROR" -ne "0" ]]; then
+    SH_MARCH=
+fi
+
+SH_PIC="-fPIC"
+PIC_ERROR=$($CC $SH_PIC -x c -c -o /dev/null - </dev/null 2>&1 | grep -i -c error)
+if [[ "$PIC_ERROR" -ne "0" ]]; then
+    SH_PIC=
+fi
+
+# For the benefit of OpenSSL. Make it run fast.
+SH_NATIVE="-march=native"
+NATIVE_ERROR=$($CC $SH_NATIVE -x c -c -o /dev/null - </dev/null 2>&1 | grep -i -c error)
+if [[ "$NATIVE_ERROR" -ne "0" ]]; then
+    SH_NATIVE=
+fi
+
+SH_DTAGS="-Wl,--enable-new-dtags"
+DT_ERROR=$($CC $SH_DTAGS -x c -c -o /dev/null - </dev/null 2>&1 | grep -i -c error)
+if [[ "$DT_ERROR" -ne "0" ]]; then
+    SH_DTAGS=
+fi
+
+# cURL CA cert path. Should be the hashes.
+if [[ -d "/etc/ssl/certs/" ]]; then
+    SH_CACERT_PATH="/etc/ssl/certs/"
+elif [[ -d "/etc/openssl/certs" ]]; then
+    SH_CACERT_PATH="/etc/openssl/certs"
+fi
+
+# cURL CA cert file. Should be the PEM concatenation.
+if [[ -f "/etc/ssl/certs/ca-bundle.crt" ]]; then
+    SH_CACERT_FILE="/etc/ssl/certs/ca-bundle.crt"
+elif [[ -f "/etc/ssl/certs/ca-certificates.crt" ]]; then
+    SH_CACERT_FILE="/etc/ssl/certs/ca-certificates.crt"
+elif [[ -f "/etc/openssl/certs/cacert.pem" ]]; then
+    SH_CACERT_FILE="/etc/openssl/certs/cacert.pem"
+fi
+
+###############################################################################
+
+OPT_PKGCONFIG=("$INSTALL_LIBDIR/pkgconfig")
+OPT_CPPFLAGS=("-I$INSTALL_PREFIX/include" "-DNDEBUG")
+OPT_CFLAGS=("$SH_MARCH" "$SH_NATIVE")
+OPT_CXXFLAGS=("$SH_MARCH" "$SH_NATIVE")
+OPT_LDFLAGS=("$SH_MARCH" "-Wl,-rpath,$INSTALL_LIBDIR" "-L$INSTALL_LIBDIR")
+OPT_LIBS=("-ldl" "-lpthread")
+
+if [[ ! -z "$SH_DTAGS" ]]; then
+    OPT_LDFLAGS+=("$SH_DTAGS")
+fi
+
+echo ""
+echo "Common flags and options:"
+echo "  PKGCONFIG: ${OPT_PKGCONFIG[*]}"
+echo "   CPPFLAGS: ${OPT_CPPFLAGS[*]}"
+echo "     CFLAGS: ${OPT_CFLAGS[*]}"
+echo "   CXXFLAGS: ${OPT_CXXFLAGS[*]}"
+echo "    LDFLAGS: ${OPT_LDFLAGS[*]}"
+echo "     LDLIBS: ${OPT_LIBS[*]}"
+
+###############################################################################
+
+# If IS_EXPORTED=1, then it was set in the parent shell
+IS_EXPORTED=$(export | grep -c SUDO_PASSWWORD)
+if [[ "$IS_EXPORTED" -eq "0" ]]; then
+
+  echo
+  echo "If you enter a sudo password, then it will be used for installation."
+  echo "If you don't enter a password, then ensure INSTALL_PREFIX is writable."
+  echo "To avoid sudo and the password, just press ENTER and they won't be used."
+  read -r -s -p "Please enter password for sudo: " SUDO_PASSWWORD
+  echo
+
+  # If IS_EXPORTED=2, then we unset it after we are done
+  export SUDO_PASSWWORD
+  IS_EXPORTED=2
+fi
+
+###############################################################################
+
+if ! ./build-zlib.sh
+then
+    echo "Failed to build zLib"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+###############################################################################
+
+if ! ./build-bzip.sh
+then
+    echo "Failed to build Bzip2"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+###############################################################################
+
+if ! ./build-iconv.sh
+then
+    echo "Failed to build iConv"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+###############################################################################
+
+if ! ./build-idn.sh
+then
+    echo "Failed to build IDN"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+###############################################################################
+
+if ! ./build-pcre.sh
+then
+    echo "Failed to build PCRE and PCRE2"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+###############################################################################
+
+echo
+echo "********** cURL **********"
+echo
+
+wget --ca-certificate="$CURL_CA_ZOO" "https://curl.haxx.se/download/$CURL_TAR" -O "$CURL_TAR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to download cURL"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+rm -rf "$CURL_DIR" &>/dev/null
+gzip -d < "$CURL_TAR" | tar xf -
+cd "$CURL_DIR"
+
+CURL_CONFIG_OPTIONS=("--enable-shared" "--enable-static" "--enable-optimize" "--enable-symbol-hiding"
+                     "--enable-http" "--enable-ftp" "--enable-file" "--enable-ldap" "--enable-ldaps"
+                     "--enable-rtsp" "--enable-proxy" "--enable-dict" "--enable-telnet" "--enable-tftp"
+                     "--enable-pop3" "--enable-imap" "--enable-smb" "--enable-smtp" "--enable-gopher"
+                     "--enable-cookies" "--enable-ipv6"
+                     "--with-zlib=$INSTALL_PREFIX" "--with-ssl=$INSTALL_PREFIX" "--without-gnutls"
+                     "--without-polarssl" "--without-mbedtls" "--without-cyassl" "--without-nss"
+                     "--without-libssh2" "--with-libidn2=$INSTALL_PREFIX" "--with-nghttp2")
+
+if [[ ! -z "$SH_CACERT_PATH" ]]; then
+    CURL_CONFIG_OPTIONS+=("--with-ca-path=$SH_CACERT_PATH")
+else
+    CURL_CONFIG_OPTIONS+=("--without-ca-path")
+fi
+
+if [[ ! -z "$SH_CACERT_FILE" ]]; then
+    CURL_CONFIG_OPTIONS+=("--with-ca-bundle=$SH_CACERT_FILE")
+else
+    CURL_CONFIG_OPTIONS+=("--without-ca-bundle")
+fi
+
+    PKG_CONFIG_PATH="${OPT_PKGCONFIG[*]}" \
+    CPPFLAGS="${OPT_CPPFLAGS[*]}" \
+    CFLAGS="${OPT_CFLAGS[*]}" CXXFLAGS="${OPT_CXXFLAGS[*]}" \
+    LDFLAGS="${OPT_LDFLAGS[*]}" LIBS="-lidn2 -lssl -lcrypto -lz ${OPT_LIBS[*]}" \
+./configure --prefix="$INSTALL_PREFIX" --libdir="$INSTALL_LIBDIR" \
+    "${CURL_CONFIG_OPTIONS[@]}"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to configure cURL"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=("-j" "$MAKE_JOBS")
+if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build cURL"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+# MAKE_FLAGS=("check")
+# if ! "$MAKE" "${MAKE_FLAGS[@]}"
+# then
+#     echo "Failed to test cURL"
+#     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+# fi
+
+MAKE_FLAGS=("install")
+if [[ ! (-z "$SUDO_PASSWWORD") ]]; then
+    echo "$SUDO_PASSWWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd "$CURR_DIR"
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true; then
+
+    ARTIFACTS=("$CURL_TAR" "$CURL_DIR")
+
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-curl.sh 2>&1 | tee build-curl.log
+    if [[ -e build-curl.log ]]; then
+        rm build-curl.log
+    fi
+fi
+
+# If IS_EXPORTED=2, then we set it
+if [[ "$IS_EXPORTED" -eq "2" ]]; then
+    unset SUDO_PASSWORD
+fi
+
+[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 0 || return 0
---
+++ build-guile.sh
@@ -0,0 +1,257 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds zLib from sources.
+
+# See fixup for INSTALL_LIBDIR below
+INSTALL_PREFIX=/usr/local
+INSTALL_LIBDIR="$INSTALL_PREFIX/lib64"
+
+GUILE_TAR=guile-2.2.2.tar.xz
+GUILE_DIR=guile-2.2.2
+
+# Avoid shellcheck.net warning
+CURR_DIR="$PWD"
+
+# Sets the number of make jobs if not set in environment
+: "${MAKE_JOBS:=4}"
+
+###############################################################################
+
+# Autotools on Solaris has an implied requirement for GNU gear. Things fall apart without it.
+# Also see https://blogs.oracle.com/partnertech/entry/preparing_for_the_upcoming_removal.
+if [[ -d "/usr/gnu/bin" ]]; then
+    if [[ ! ("$PATH" == *"/usr/gnu/bin"*) ]]; then
+        echo
+        echo "Adding /usr/gnu/bin to PATH for Solaris"
+        PATH="/usr/gnu/bin:$PATH"
+    fi
+elif [[ -d "/usr/swf/bin" ]]; then
+    if [[ ! ("$PATH" == *"/usr/sfw/bin"*) ]]; then
+        echo
+        echo "Adding /usr/sfw/bin to PATH for Solaris"
+        PATH="/usr/sfw/bin:$PATH"
+    fi
+elif [[ -d "/usr/ucb/bin" ]]; then
+    if [[ ! ("$PATH" == *"/usr/ucb/bin"*) ]]; then
+        echo
+        echo "Adding /usr/ucb/bin to PATH for Solaris"
+        PATH="/usr/ucb/bin:$PATH"
+    fi
+fi
+
+###############################################################################
+
+if [[ -z $(command -v gzip 2>/dev/null) ]]; then
+    echo "Some packages gzip. Please install gzip."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+IS_DARWIN=$(uname -s | grep -i -c darwin)
+if [[ ("$IS_DARWIN" -eq "0") ]] && [[ -z $(command -v libtoolize 2>/dev/null) ]]; then
+    echo "Some packages require libtool. Please install libtool or libtool-bin."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+if [[ -z $(command -v autoreconf 2>/dev/null) ]]; then
+    echo "Some packages require autoreconf. Please install autoconf or automake."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+if [[ ! -f "$HOME/.cacert/lets-encrypt-root-x3.pem" ]]; then
+    echo "zLib requires several CA roots. Please run build-cacert.sh."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+LETS_ENCRYPT_ROOT="$HOME/.cacert/lets-encrypt-root-x3.pem"
+IDENTRUST_ROOT="$HOME/.cacert/identrust-root-x3.pem"
+
+###############################################################################
+
+THIS_SYSTEM=$(uname -s 2>&1)
+IS_DARWIN=$(echo -n "$THIS_SYSTEM" | grep -i -c darwin)
+IS_SOLARIS=$(echo -n "$THIS_SYSTEM" | grep -i -c sunos)
+
+# The BSDs and Solaris should have GMake installed if its needed
+if [[ $(command -v gmake 2>/dev/null) ]]; then
+    MAKE="gmake"
+else
+    MAKE="make"
+fi
+
+# Try to determine 32 vs 64-bit, /usr/local/lib, /usr/local/lib32 and /usr/local/lib64
+# The Autoconf programs misdetect Solaris as x86 even though its x64. OpenBSD has
+# getconf, but it does not have LONG_BIT.
+IS_64BIT=$(getconf LONG_BIT 2>&1 | grep -i -c 64)
+if [[ "$IS_64BIT" -eq "0" ]]; then
+    IS_64BIT=$(file /bin/ls 2>&1 | grep -i -c '64-bit')
+fi
+
+if [[ "$IS_SOLARIS" -ne "0" ]]; then
+    SH_MARCH="-m64"
+    INSTALL_LIBDIR="$INSTALL_PREFIX/lib64"
+elif [[ "$IS_64BIT" -ne "0" ]]; then
+    if [[ (-d /usr/lib) && (-d /usr/lib32) ]]; then
+        SH_MARCH="-m64"
+        INSTALL_LIBDIR="$INSTALL_PREFIX/lib"
+    elif [[ (-d /usr/lib) && (-d /usr/lib64) ]]; then
+        SH_MARCH="-m64"
+        INSTALL_LIBDIR="$INSTALL_PREFIX/lib64"
+    else
+        SH_MARCH="-m64"
+        INSTALL_LIBDIR="$INSTALL_PREFIX/lib"
+    fi
+else
+    SH_MARCH="-m32"
+    INSTALL_LIBDIR="$INSTALL_PREFIX/lib"
+fi
+
+if [[ (-z "$CC" && $(command -v cc 2>/dev/null) ) ]]; then CC=$(command -v cc); fi
+if [[ (-z "$CXX" && $(command -v CC 2>/dev/null) ) ]]; then CXX=$(command -v CC); fi
+
+MARCH_ERROR=$($CC $SH_MARCH -x c -c -o /dev/null - </dev/null 2>&1 | grep -i -c error)
+if [[ "$MARCH_ERROR" -ne "0" ]]; then
+    SH_MARCH=
+fi
+
+SH_PIC="-fPIC"
+PIC_ERROR=$($CC $SH_PIC -x c -c -o /dev/null - </dev/null 2>&1 | grep -i -c error)
+if [[ "$PIC_ERROR" -ne "0" ]]; then
+    SH_PIC=
+fi
+
+# For the benefit of Guile. Make it run fast.
+SH_NATIVE="-march=native"
+NATIVE_ERROR=$($CC $SH_NATIVE -x c -c -o /dev/null - </dev/null 2>&1 | grep -i -c error)
+if [[ "$NATIVE_ERROR" -ne "0" ]]; then
+    SH_NATIVE=
+fi
+
+SH_DTAGS="-Wl,--enable-new-dtags"
+DT_ERROR=$($CC $SH_DTAGS -x c -c -o /dev/null - </dev/null 2>&1 | grep -i -c error)
+if [[ "$DT_ERROR" -ne "0" ]]; then
+    SH_DTAGS=
+fi
+
+###############################################################################
+
+OPT_PKGCONFIG=("$INSTALL_LIBDIR/pkgconfig")
+OPT_CPPFLAGS=("-I$INSTALL_PREFIX/include" "-DNDEBUG")
+OPT_CFLAGS=("$SH_MARCH" "$SH_NATIVE")
+OPT_CXXFLAGS=("$SH_MARCH" "$SH_NATIVE")
+OPT_LDFLAGS=("$SH_MARCH" "-Wl,-rpath,$INSTALL_LIBDIR" "-L$INSTALL_LIBDIR")
+OPT_LIBS=("-ldl" "-lpthread")
+
+OPT_CFLAGS+=("-fsanitize=undefined")
+OPT_CXXFLAGS+=("-fsanitize=undefined")
+OPT_LDFLAGS+=("-lubsan")
+
+if [[ ! -z "$SH_DTAGS" ]]; then
+    OPT_LDFLAGS+=("$SH_DTAGS")
+fi
+
+###############################################################################
+
+# If IS_EXPORTED=1, then it was set in the parent shell
+IS_EXPORTED=$(export | grep -c SUDO_PASSWWORD)
+if [[ "$IS_EXPORTED" -eq "0" ]]; then
+
+  echo
+  echo "If you enter a sudo password, then it will be used for installation."
+  echo "If you don't enter a password, then ensure INSTALL_PREFIX is writable."
+  echo "To avoid sudo and the password, just press ENTER and they won't be used."
+  read -r -s -p "Please enter password for sudo: " SUDO_PASSWWORD
+  echo
+
+  # If IS_EXPORTED=2, then we unset it after we are done
+  export SUDO_PASSWWORD
+  IS_EXPORTED=2
+fi
+
+###############################################################################
+
+echo
+echo "********** Guile **********"
+echo
+
+wget --ca-certificate="$IDENTRUST_ROOT" "https://ftp.gnu.org/gnu/guile/$GUILE_TAR" -O "$GUILE_TAR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to download Guile"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+rm -rf "$GUILE_DIR" &>/dev/null
+tar xJf "$GUILE_TAR"
+cd "$GUILE_DIR"
+
+# Rebuild libtool, http://stackoverflow.com/q/35589427/608639
+autoconf
+
+# http://pkgs.fedoraproject.org/cgit/rpms/gnutls.git/tree/gnutls.spec; thanks NM.
+if [[ "$IS_LINUX" -ne "0" ]]; then
+    sed -i -e 's|sys_lib_dlsearch_path_spec="/lib /usr/lib|sys_lib_dlsearch_path_spec="/lib %{_libdir} /usr/lib|g' configure
+fi
+
+    PKG_CONFIG_PATH="${OPT_PKGCONFIG[*]}" \
+    CPPFLAGS="${OPT_CPPFLAGS[*]}" \
+    CFLAGS="${OPT_CFLAGS[*]}" CXXFLAGS="${OPT_CXXFLAGS[*]}" \
+    LDFLAGS="${OPT_LDFLAGS[*]}" LIBS="${OPT_LIBS[*]}" \
+./configure --enable-shared --prefix="$INSTALL_PREFIX" --libdir="$INSTALL_LIBDIR" \
+    --with-libgmp-prefix="$INSTALL_PREFIX" \
+    --with-libunistring-prefix="$INSTALL_PREFIX" \
+    --with-libiconv-prefix="$INSTALL_PREFIX" \
+    --with-readline-prefix="$INSTALL_PREFIX"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to configure Guile"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=("-j" "$MAKE_JOBS" "V=1")
+if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build Guile"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+# https://lists.gnu.org/archive/html/guile-devel/2017-10/msg00009.html
+# MAKE_FLAGS=("check" "V=1")
+# if ! "$MAKE" "${MAKE_FLAGS[@]}"
+# then
+#     echo "Failed to test Guile"
+#     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+# fi
+
+MAKE_FLAGS=("install")
+if [[ ! (-z "$SUDO_PASSWWORD") ]]; then
+    echo "$SUDO_PASSWWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd "$CURR_DIR"
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true; then
+
+    ARTIFACTS=("$GUILE_TAR" "$GUILE_DIR")
+
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-guile.sh 2>&1 | tee build-guile.log
+    if [[ -e build-guile.log ]]; then
+        rm build-guile.log
+    fi
+fi
+
+# If IS_EXPORTED=2, then we set it
+if [[ "$IS_EXPORTED" -eq "2" ]]; then
+    unset SUDO_PASSWORD
+fi
+
+[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 0 || return 0
