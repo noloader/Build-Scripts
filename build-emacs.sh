53a6f4063b8714ce5378732a3c8bc443173cd0b5
Jeffrey Walton <noloader@gmail.com>
2017-10-23 04:44:24 -0400

    Cleanup build-emacs.sh

--- build-emacs.sh
+++ build-emacs.sh
@@ -19,8 +19,8 @@ EMACS_DIR=emacs-24.5
 # Avoid shellcheck.net warning
 CURR_DIR="$PWD"
 
-# Sets the number of make jobs
-MAKE_JOBS=4
+# Sets the number of make jobs if not set in environment
+: "${MAKE_JOBS:=4}"
 
 ###############################################################################
 
@@ -53,18 +53,34 @@ if [[ -z $(command -v gzip 2>/dev/null) ]]; then
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
+if [[ -z $(command -v bzip2 2>/dev/null) ]]; then
+    echo "Some packages bzip2. Please install bzip2."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+IS_DARWIN=$(uname -s | grep -i -c darwin)
+if [[ ("$IS_DARWIN" -eq "0") ]] && [[ -z $(command -v libtoolize 2>/dev/null) ]]; then
+    echo "Some packages require libtool. Please install libtool or libtool-bin."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
 if [[ -z $(command -v autoreconf 2>/dev/null) ]]; then
     echo "Some packages require autoreconf. Please install autoconf or automake."
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
 if [[ ! -f "$HOME/.cacert/lets-encrypt-root-x3.pem" ]]; then
-    echo "Wget requires several CA roots. Please run build-cacert.sh."
+    echo "Emacs requires several CA roots. Please run build-cacert.sh."
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
 if [[ ! -f "$HOME/.cacert/identrust-root-x3.pem" ]]; then
-    echo "Wget requires several CA roots. Please run build-cacert.sh."
+    echo "Emacs requires several CA roots. Please run build-cacert.sh."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+if [[ ! -f "$HOME/.cacert/digicert-root-ca.pem" ]]; then
+    echo "Emacs requires several CA roots. Please run build-cacert.sh."
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
@@ -73,25 +89,11 @@ IDENTRUST_ROOT="$HOME/.cacert/identrust-root-x3.pem"
 
 ###############################################################################
 
-echo
-echo "If you enter a sudo password, then it will be used for installation."
-echo "If you don't enter a password, then ensure INSTALL_PREFIX is writable."
-echo "To avoid sudo and the password, just press ENTER and they won't be used."
-read -r -s -p "Please enter password for sudo: " SUDO_PASSWWORD
-echo
-
-###############################################################################
-
 THIS_SYSTEM=$(uname -s 2>&1)
 IS_DARWIN=$(echo -n "$THIS_SYSTEM" | grep -i -c darwin)
-IS_LINUX=$(echo -n "$THIS_SYSTEM" | grep -i -c linux)
 IS_CYGWIN=$(echo -n "$THIS_SYSTEM" | grep -i -c cygwin)
-IS_MINGW=$(echo -n "$THIS_SYSTEM" | grep -i -c mingw)
-IS_OPENBSD=$(echo -n "$THIS_SYSTEM" | grep -i -c openbsd)
-IS_DRAGONFLY=$(echo -n "$THIS_SYSTEM" | grep -i -c dragonfly)
-IS_FREEBSD=$(echo -n "$THIS_SYSTEM" | grep -i -c freebsd)
-IS_NETBSD=$(echo -n "$THIS_SYSTEM" | grep -i -c netbsd)
 IS_SOLARIS=$(echo -n "$THIS_SYSTEM" | grep -i -c sunos)
+IS_FEDORA=$(lsb_release -a 2>/dev/null | grep -i -c 'Fedora')
 
 # The BSDs and Solaris should have GMake installed if its needed
 if [[ $(command -v gmake 2>/dev/null) ]]; then
@@ -109,39 +111,26 @@ if [[ "$IS_64BIT" -eq "0" ]]; then
 fi
 
 if [[ "$IS_SOLARIS" -ne "0" ]]; then
-    SH_KBITS="64"
     SH_MARCH="-m64"
     INSTALL_LIBDIR="$INSTALL_PREFIX/lib64"
-    INSTALL_LIBDIR_DIR="lib64"
 elif [[ "$IS_64BIT" -ne "0" ]]; then
     if [[ (-d /usr/lib) && (-d /usr/lib32) ]]; then
-        SH_KBITS="64"
         SH_MARCH="-m64"
         INSTALL_LIBDIR="$INSTALL_PREFIX/lib"
-        INSTALL_LIBDIR_DIR="lib"
     elif [[ (-d /usr/lib) && (-d /usr/lib64) ]]; then
-        SH_KBITS="64"
         SH_MARCH="-m64"
         INSTALL_LIBDIR="$INSTALL_PREFIX/lib64"
-        INSTALL_LIBDIR_DIR="lib64"
     else
-        SH_KBITS="64"
         SH_MARCH="-m64"
         INSTALL_LIBDIR="$INSTALL_PREFIX/lib"
-        INSTALL_LIBDIR_DIR="lib"
     fi
 else
-    SH_KBITS="32"
     SH_MARCH="-m32"
     INSTALL_LIBDIR="$INSTALL_PREFIX/lib"
-    INSTALL_LIBDIR_DIR="lib"
 fi
 
 if [[ (-z "$CC" && $(command -v cc 2>/dev/null) ) ]]; then CC=$(command -v cc); fi
 if [[ (-z "$CXX" && $(command -v CC 2>/dev/null) ) ]]; then CXX=$(command -v CC); fi
-
-# Emacs uses signals, and it needs _XOPEN_SOURCE for Newlib
-IS_NEWLIB=$(echo '#include <stdlib.h>' | "$CC" -x c -dM -E - | grep -i -c "__NEWLIB__")
 
 MARCH_ERROR=$($CC $SH_MARCH -x c -c -o /dev/null - </dev/null 2>&1 | grep -i -c error)
 if [[ "$MARCH_ERROR" -ne "0" ]]; then
@@ -154,16 +143,55 @@ if [[ "$PIC_ERROR" -ne "0" ]]; then
     SH_PIC=
 fi
 
+# For the benefit of Nettle, GMP and Emacs. Make them run fast.
+SH_NATIVE="-march=native"
+NATIVE_ERROR=$($CC $SH_NATIVE -x c -c -o /dev/null - </dev/null 2>&1 | grep -i -c error)
+if [[ "$NATIVE_ERROR" -ne "0" ]]; then
+    SH_NATIVE=
+fi
+
+SH_DTAGS="-Wl,--enable-new-dtags"
+DT_ERROR=$($CC $SH_DTAGS -x c -c -o /dev/null - </dev/null 2>&1 | grep -i -c error)
+if [[ "$DT_ERROR" -ne "0" ]]; then
+    SH_DTAGS=
+fi
+
 # Solaris fixup.... Ncurses 6.0 does not build and the patches don't apply
 if [[ "$IS_SOLARIS" -ne "0" ]]; then
     NCURSES_TAR=ncurses-5.9.tar.gz
     NCURSES_DIR=ncurses-5.9
 fi
 
+###############################################################################
+
+OPT_PKGCONFIG=("$INSTALL_LIBDIR/pkgconfig")
+OPT_CPPFLAGS=("-I$INSTALL_PREFIX/include" "-DNDEBUG")
+OPT_CFLAGS=("$SH_MARCH" "$SH_NATIVE")
+OPT_CXXFLAGS=("$SH_MARCH" "$SH_NATIVE")
+OPT_LDFLAGS=("$SH_MARCH" "-Wl,-rpath,$INSTALL_LIBDIR" "-L$INSTALL_LIBDIR")
+OPT_LIBS=("-ldl" "-lpthread")
+
+if [[ ! -z "$SH_DTAGS" ]]; then
+    OPT_LDFLAGS+=("$SH_DTAGS")
+fi
+
+echo ""
+echo "Common flags and options:"
+echo "  PKGCONFIG: ${OPT_PKGCONFIG[*]}"
+echo "   CPPFLAGS: ${OPT_CPPFLAGS[*]}"
+echo "     CFLAGS: ${OPT_CFLAGS[*]}"
+echo "   CXXFLAGS: ${OPT_CXXFLAGS[*]}"
+echo "    LDFLAGS: ${OPT_LDFLAGS[*]}"
+echo "     LDLIBS: ${OPT_LIBS[*]}"
+
+###############################################################################
+
 echo
-echo "********** libdir **********"
+echo "If you enter a sudo password, then it will be used for installation."
+echo "If you don't enter a password, then ensure INSTALL_PREFIX is writable."
+echo "To avoid sudo and the password, just press ENTER and they won't be used."
+read -r -s -p "Please enter password for sudo: " SUDO_PASSWWORD
 echo
-echo "Using libdir $INSTALL_LIBDIR"
 
 ###############################################################################
 
@@ -208,7 +236,7 @@ then
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
-MAKE_FLAGS=(install)
+MAKE_FLAGS=("install")
 if [[ ! (-z "$SUDO_PASSWWORD") ]]; then
     echo "$SUDO_PASSWWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
 else
@@ -234,13 +262,19 @@ rm -rf "$NCURSES_DIR" &>/dev/null
 gzip -d < "$NCURSES_TAR" | tar xf -
 cd "$NCURSES_DIR"
 
-SH_LDLIBS=("-ldl" "-lpthread")
-SH_LDFLAGS=("$SH_MARCH" "-Wl,-rpath,$INSTALL_LIBDIR" "-L$INSTALL_LIBDIR")
-
-    CPPFLAGS="-I$INSTALL_PREFIX/include -DNDEBUG $SH_PIC" \
-    CFLAGS="$SH_MARCH" CXXFLAGS="$SH_MARCH" \
-    LDFLAGS="${SH_LDFLAGS[*]}" LIBS="${SH_LDLIBS[*]}" \
-./configure --enable-shared --prefix="$INSTALL_PREFIX" --libdir="$INSTALL_LIBDIR"
+    PKG_CONFIG_PATH="${OPT_PKGCONFIG[*]}" \
+    CPPFLAGS="${OPT_CPPFLAGS[*]}" \
+    CFLAGS="${OPT_CFLAGS[*]}" CXXFLAGS="${OPT_CXXFLAGS[*]}" \
+    LDFLAGS="${OPT_LDFLAGS[*]}" LIBS="${OPT_LIBS[*]}" \
+./configure --prefix="$INSTALL_PREFIX" --libdir="$INSTALL_LIBDIR" \
+    --with-shared --with-cxx-shared --enable-pc-files \
+    --with-termlib --enable-widec --disable-root-environ \
+    --with-build-cc="$CC" --with-build-cxx="$CXX" \
+    --with-build-cpp="${OPT_CPPFLAGS[*]}" \
+    --with-build-cflags="${OPT_CFLAGS[*]}" \
+    --with-build-cxxflags="${OPT_CXXFLAGS[*]}"  \
+    --with-build-ldflags="${OPT_LDFLAGS[*]}" \
+    --with-build-libs="${OPT_LIBS[*]}"
 
 if [[ "$?" -ne "0" ]]; then
     echo "Failed to configure ncurses"
@@ -254,7 +288,7 @@ then
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
-MAKE_FLAGS=(install)
+MAKE_FLAGS=("install")
 if [[ ! (-z "$SUDO_PASSWWORD") ]]; then
     echo "$SUDO_PASSWWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
 else
@@ -308,14 +342,14 @@ if [[ "$?" -ne "0" ]]; then
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
-MAKE_FLAGS=("-j" "$MAKE_JOBS" all)
+MAKE_FLAGS=("-j" "$MAKE_JOBS" "all")
 if ! "$MAKE" "${MAKE_FLAGS[@]}"
 then
     echo "Failed to build emacs"
     [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
 fi
 
-MAKE_FLAGS=(install)
+MAKE_FLAGS=("install")
 if [[ ! (-z "$SUDO_PASSWWORD") ]]; then
     echo "$SUDO_PASSWWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
 else
