109bcdb3c60b07ca234a64855aa172715ffe426d
Jeffrey Walton <noloader@gmail.com>
2017-11-05 05:05:14 -0500

    Add GetText as build dependency for Git

--- build-git.sh
+++ build-git.sh
@@ -122,6 +122,14 @@ fi
 
 ###############################################################################
 
+if ! ./build-gettext.sh
+then
+    echo "Failed to build iConv"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+###############################################################################
+
 if ! ./build-idn.sh
 then
     echo "Failed to build IDN"
