d9e82a028788678be19b0266a583488a15e8d1c6
Jeffrey Walton <noloader@gmail.com>
2018-01-04 23:44:53 -0500

    Update clean program

--- clean-autotools.sh
+++ clean-autotools.sh
@@ -6,28 +6,85 @@
 
 # Run the script like so:
 #
-# sudo ./clean-autotools.sh
+#    sudo ./clean-autotools.sh
+
+# The extra gyrations around Perl files are due to Perl's
+# Autom4te::ChannelDefs getting whacked along with the
+# Autotools files. Its non-trivial to reinstall the missing
+# Perl files because the sources must be compiled again.
 
 echo "Cleaning autom4te..."
-find /usr/local -name 'autom4te' -exec rm -rf {} \; 2>/dev/null
+FILES=$(find /usr/local -name 'autom4te' 2>/dev/null)
+for file in "${FILES[*]}"; do
+    DONT_DELETE=$(echo "$file" | grep -c -e '\.pm')
+    if [[ "$DONT_DELETE" -eq "0" ]]; then
+        rm -f "$file"
+    fi
+done
 
 echo "Cleaning autopoint..."
-find /usr/local -name 'autopoint' -exec rm -rf {} \; 2>/dev/null
+FILES=$(find /usr/local -name 'autopoint' 2>/dev/null)
+for file in "${FILES[*]}"; do
+    DONT_DELETE=$(echo "$file" | grep -c -e '\.pm')
+    if [[ "$DONT_DELETE" -eq "0" ]]; then
+        rm -f "$file"
+    fi
+done
 
 echo "Cleaning autoconf..."
-find /usr/local -name 'autoconf' -exec rm -rf {} \; 2>/dev/null
+FILES=$(find /usr/local -name 'autoconf' 2>/dev/null)
+for file in "${FILES[*]}"; do
+    DONT_DELETE=$(echo "$file" | grep -c -e '\.pm')
+    if [[ "$DONT_DELETE" -eq "0" ]]; then
+        rm -f "$file"
+    fi
+done
+
+echo "Cleaning autoconf..."
+FILES=$(find /usr/local -name 'autoreconf' 2>/dev/null)
+for file in "${FILES[*]}"; do
+    DONT_DELETE=$(echo "$file" | grep -c -e '\.pm')
+    if [[ "$DONT_DELETE" -eq "0" ]]; then
+        rm -f "$file"
+    fi
+done
 
 echo "Cleaning autoheader..."
-find /usr/local -name 'autoheader' -exec rm -rf {} \;
+FILES=$(find /usr/local -name 'autoheader' 2>/dev/null)
+for file in "${FILES[*]}"; do
+    DONT_DELETE=$(echo "$file" | grep -c -e '\.pm')
+    if [[ "$DONT_DELETE" -eq "0" ]]; then
+        rm -f "$file"
+    fi
+done
 
 echo "Cleaning automake..."
-find /usr/local -name 'automake' -exec rm -rf {} \;
+FILES=$(find /usr/local -name 'automake' 2>/dev/null)
+for file in "${FILES[*]}"; do
+    DONT_DELETE=$(echo "$file" | grep -c -e '\.pm')
+    if [[ "$DONT_DELETE" -eq "0" ]]; then
+        rm -f "$file"
+    fi
+done
 
 echo "Cleaning autoupdate..."
-find /usr/local -name 'autoupdate' -exec rm -rf {} \;
+FILES=$(find /usr/local -name 'autoupdate' 2>/dev/null)
+for file in "${FILES[*]}"; do
+    DONT_DELETE=$(echo "$file" | grep -c -e '\.pm')
+    if [[ "$DONT_DELETE" -eq "0" ]]; then
+        rm -f "$file"
+    fi
+done
 
 echo "Cleaning aclocal..."
-find /usr/local -name 'aclocal*' -exec rm -rf {} \;
+FILES=$(find /usr/local -name 'aclocal*' 2>/dev/null)
+for file in "${FILES[*]}"; do
+    DONT_DELETE=$(echo "$file" | grep -c -e '\.pm')
+    if [[ "$DONT_DELETE" -eq "0" ]]; then
+        rm -f "$file"
+    fi
+done
 
-echo "Done. Please update your shell's program cache."
+echo "You may need to update libtool if you did so previously."
+echo "Please update your shell's program cache."
 [[ "$0" = "${BASH_SOURCE[0]}" ]] && hash -r
