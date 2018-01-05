4bc28ee23748d44f04cfb66b3cb23bb795fc4d77
Jeffrey Walton <noloader@gmail.com>
2018-01-05 01:10:23 -0500

    Add ICU 60.2 library script

---
+++ build-icu.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds ICU from sources.
+
+# ICU 60.2
+ICU_TAR=icu4c-60_2-src.tgz
+ICU_DIR=icu
+
+# Avoid shellcheck.net warning
+CURR_DIR="$PWD"
+
+# Sets the number of make jobs if not set in environment
+: "${MAKE_JOBS:=4}"
+
+###############################################################################
+
+if [[ -z $(command -v autoreconf 2>/dev/null) ]]; then
+    echo "Some packages require autoreconf. Please install autoconf or automake."
+    echo "You can run build-autotools.sh and build-libtool.sh to update them."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+if [[ -z $(command -v gzip 2>/dev/null) ]]; then
+    echo "Some packages require gzip. Please install gzip."
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+###############################################################################
+
+# Get environment if needed. We can't export it because it includes arrays.
+if [[ -z "$BUILD_OPTS" ]]; then
+    source ./build-environ.sh
+fi
+
+# The password should die when this subshell goes out of scope
+if [[ -z "$SUDO_PASSWORD" ]]; then
+    source ./build-password.sh
+fi
+
+###############################################################################
+
+echo
+echo "********** libicu **********"
+echo
+
+# http://download.icu-project.org/files/icu4c/60.2/icu4c-60_2-src.tgz
+wget "http://download.icu-project.org/files/icu4c/60.2/$ICU_TAR" -O "$ICU_TAR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to download libicu"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+rm -rf "$ICU_DIR" &>/dev/null
+gzip -d < "$ICU_TAR" | tar xf -
+cd "$ICU_DIR/source"
+
+# http://pkgs.fedoraproject.org/cgit/rpms/gnutls.git/tree/gnutls.spec; thanks NM.
+# AIX needs the execute bit reset on the file.
+sed -e 's|sys_lib_dlsearch_path_spec="/lib /usr/lib|sys_lib_dlsearch_path_spec="/lib %{_libdir} /usr/lib|g' configure > configure.fixed
+mv configure.fixed configure; chmod +x configure
+
+    PKG_CONFIG_PATH="${BUILD_PKGCONFIG[*]}" \
+    CPPFLAGS="${BUILD_CPPFLAGS[*]}" \
+    CFLAGS="${BUILD_CFLAGS[*]}" CXXFLAGS="${BUILD_CXXFLAGS[*]}" \
+    LDFLAGS="${BUILD_LDFLAGS[*]}" LIBS="${BUILD_LIBS[*]}" \
+./configure --enable-shared --enable-static \
+    --prefix="$INSTALL_PREFIX" --libdir="$INSTALL_LIBDIR" \
+    --with-library-bits="$SH_BITS" \
+    --with-data-packaging=auto
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to configure libicu"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=("-j" "$MAKE_JOBS")
+if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build libicu"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+ MAKE_FLAGS=("check")
+ if ! "$MAKE" "${MAKE_FLAGS[@]}"
+ then
+    echo "Failed to test libicu"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+ fi
+
+MAKE_FLAGS=("install")
+if [[ ! (-z "$SUDO_PASSWORD") ]]; then
+    echo "$SUDO_PASSWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd "$CURR_DIR"
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true; then
+
+    ARTIFACTS=("$ICU_TAR" "$ICU_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-icu.sh 2>&1 | tee build-icu.log
+    if [[ -e build-icu.log ]]; then
+        rm -f build-icu.log
+    fi
+fi
+
+[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 0 || return 0
