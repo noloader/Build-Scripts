87682e122e2c354b76f85b97e9230c45fa3f133d
Jeffrey Walton <noloader@gmail.com>
2018-02-02 16:10:28 -0500

    Update clean script

--- clean-docs.sh
+++ clean-docs.sh
@@ -11,7 +11,7 @@ PACKAGES=(
 	automake b2sum bzip cacert clamav
 	cryptopp curl emacs environ expat
 	gettext git gmp gnutls guile
-	gzip iconv icu idn less
+	gzip iconv icu idn idn2 less
 	libffi libtool mawk ncurses nettle
 	openssl openvpn p11kit pcre	perl
 	psl readline sed ssh tar
@@ -27,7 +27,15 @@ echo ""
 echo "Cleaing shared documentation"
 
 for dir in "${PACKAGES[@]}"; do
-    find /usr/local/man -type d -name "$dir*" -exec rm -rf {} \; 2>/dev/null
+    find /usr/local/man -type d -iname "$dir*" -exec rm -rf {} \; 2>/dev/null
+done
+
+for dir in "${PACKAGES[@]}"; do
+    find /usr/local/share/man -type d -iname "$dir*" -exec rm -rf {} \; 2>/dev/null
+done
+
+for file in "${PACKAGES[@]}"; do
+    find /usr/local/share/man -type f -iname "*$file*" -exec rm -f {} \; 2>/dev/null
 done
 
 [[ "$0" = "${BASH_SOURCE[0]}" ]] && hash -r
