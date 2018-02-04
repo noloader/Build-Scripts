0550b74eedf0cfae706eebc7f0b0bf6e18d1caab
Jeffrey Walton <noloader@gmail.com>
2018-02-03 20:03:38 -0500

    Add build-make.sh script

---
+++ build-make.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds Make from sources.
+
+MAKE_TAR=make-4.2.1.tar.gz
+MAKE_DIR=make-4.2.1
+
+# Avoid shellcheck.net warning
+CURR_DIR="$PWD"
+
+# Sets the number of make jobs if not set in environment
+: "${MAKE_JOBS:=4}"
+
+###############################################################################
+
+# Get environment if needed. We can't export it because it includes arrays.
+source ./build-environ.sh
+
+# The password should die when this subshell goes out of scope
+if [[ -z "$SUDO_PASSWORD" ]]; then
+    source ./build-password.sh
+fi
+
+###############################################################################
+
+if ! ./build-iconv.sh
+then
+    echo "Failed to build iConv"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+###############################################################################
+
+echo
+echo "********** Make **********"
+echo
+
+wget --ca-certificate="$IDENTRUST_ROOT" "https://ftp.gnu.org/gnu/make/$MAKE_TAR" -O "$MAKE_TAR"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to download Make"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+rm -rf "$MAKE_DIR" &>/dev/null
+gzip -d < "$MAKE_TAR" | tar xf -
+cd "$MAKE_DIR"
+
+# http://pkgs.fedoraproject.org/cgit/rpms/gnutls.git/tree/gnutls.spec; thanks NM.
+# AIX needs the execute bit reset on the file.
+sed -e 's|sys_lib_dlsearch_path_spec="/lib /usr/lib|sys_lib_dlsearch_path_spec="/lib %{_libdir} /usr/lib|g' configure > configure.fixed
+mv configure.fixed configure; chmod +x configure
+
+    PKG_CONFIG_PATH="${BUILD_PKGCONFIG[*]}" \
+    CPPFLAGS="${BUILD_CPPFLAGS[*]}" \
+    CFLAGS="${BUILD_CFLAGS[*]}" \
+    CXXFLAGS="${BUILD_CXXFLAGS[*]}" \
+    LDFLAGS="${BUILD_LDFLAGS[*]}" \
+    LIBS="${BUILD_LIBS[*]}" \
+./configure --prefix="$INSTX_PREFIX" --libdir="$INSTX_LIBDIR" \
+    --with-libiconv-prefix="$INSTX_PREFIX" \
+    --with-libintl-prefix="$INSTX_PREFIX"
+
+if [[ "$?" -ne "0" ]]; then
+    echo "Failed to configure Make"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=("-j" "$MAKE_JOBS")
+if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build Make"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+# ??? "Can't locate test_driver.pl"
+MAKE_FLAGS=("PERL_USE_UNSAFE_INC=1" "check")
+if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to test Make"
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
+fi
+
+MAKE_FLAGS=("install")
+if [[ ! (-z "$SUDO_PASSWORD") ]]; then
+    echo "$SUDO_PASSWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd "$CURR_DIR"
+
+###############################################################################
+
+echo ""
+echo "*****************************************************************************"
+echo "Please run Bash's 'hash -r' to update program cache in the current shell"
+echo "*****************************************************************************"
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true; then
+
+    ARTIFACTS=("$MAKE_TAR" "$MAKE_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-make.sh 2>&1 | tee build-make.log
+    if [[ -e build-make.log ]]; then
+        rm -f build-make.log
+    fi
+fi
+
+[[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 0 || return 0
