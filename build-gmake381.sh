f7a8297cf3ecddcf05bcf500c39fb8208fb419b6
Jeffrey Walton <noloader@gmail.com>
2021-01-14 15:36:13 -0500

    Add Gmake 3.81 recipe

---
+++ build-gmake381.sh
@@ -0,0 +1,169 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds Make from sources.
+
+MAKE_VER=3.81
+MAKE_TAR=make-${MAKE_VER}.tar.gz
+MAKE_DIR=make-${MAKE_VER}
+PKG_NAME=make-381
+
+###############################################################################
+
+# Get the environment as needed.
+if [[ "${SETUP_ENVIRON_DONE}" != "yes" ]]; then
+    if ! source ./setup-environ.sh
+    then
+        echo "Failed to set environment"
+        exit 1
+    fi
+fi
+
+# The password should die when this subshell goes out of scope
+if [[ "${SUDO_PASSWORD_DONE}" != "yes" ]]; then
+    if ! source ./setup-password.sh
+    then
+        echo "Failed to process password"
+        exit 1
+    fi
+fi
+
+###############################################################################
+
+if ! ./build-cacert.sh
+then
+    echo "Failed to install CA Certs"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-base.sh
+then
+    echo "Failed to build GNU base packages"
+    exit 1
+fi
+
+###############################################################################
+
+echo ""
+echo "========================================"
+echo "================= Make ================="
+echo "========================================"
+
+echo ""
+echo "**********************"
+echo "Downloading package"
+echo "**********************"
+
+if ! "$WGET" -q -O "$MAKE_TAR" --ca-certificate="$LETS_ENCRYPT_ROOT" \
+     "https://ftp.gnu.org/gnu/make/$MAKE_TAR"
+then
+    echo "Failed to download Make"
+    exit 1
+fi
+
+rm -rf "$MAKE_DIR" &>/dev/null
+gzip -d < "$MAKE_TAR" | tar xf -
+cd "$MAKE_DIR"
+
+if [[ -e ../patch/gmake381.patch ]]; then
+    patch -u -p0 < ../patch/gmake381.patch
+    echo ""
+fi
+
+# Fix sys_lib_dlsearch_path_spec
+bash ../fix-configure.sh
+
+echo "**********************"
+echo "Configuring package"
+echo "**********************"
+
+    PKG_CONFIG_PATH="${INSTX_PKGCONFIG}" \
+    CPPFLAGS="${INSTX_CPPFLAGS}" \
+    ASFLAGS="${INSTX_ASFLAGS}" \
+    CFLAGS="${INSTX_CFLAGS}" \
+    CXXFLAGS="${INSTX_CXXFLAGS}" \
+    LDFLAGS="${INSTX_LDFLAGS}" \
+    LIBS="${INSTX_LDLIBS}" \
+./configure \
+    --build="${AUTOCONF_BUILD}" \
+    --prefix="${INSTX_PREFIX}" \
+    --libdir="${INSTX_LIBDIR}" \
+    --program-prefix=g \
+    --with-libiconv-prefix="${INSTX_PREFIX}" \
+    --with-libintl-prefix="${INSTX_PREFIX}"
+
+if [[ "$?" -ne 0 ]]; then
+    echo "Failed to configure Make"
+    exit 1
+fi
+
+# Escape dollar sign for $ORIGIN in makefiles. Required so
+# $ORIGIN works in both configure tests and makefiles.
+bash ../fix-makefiles.sh
+
+echo "**********************"
+echo "Building package"
+echo "**********************"
+
+MAKE_FLAGS=("-j" "${INSTX_JOBS}")
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build Make"
+    exit 1
+fi
+
+# Fix flags in *.pc files
+bash ../fix-pkgconfig.sh
+
+echo "**********************"
+echo "Testing package"
+echo "**********************"
+
+# Can't pass self tests...
+MAKE_FLAGS=("PERL_USE_UNSAFE_INC=1" "check")
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to test Make"
+    #exit 1
+fi
+
+echo "**********************"
+echo "Installing package"
+echo "**********************"
+
+MAKE_FLAGS=("install")
+if [[ -n "$SUDO_PASSWORD" ]]; then
+    printf "%s\n" "$SUDO_PASSWORD" | sudo ${SUDO_ENV_OPT} -S "${MAKE}" "${MAKE_FLAGS[@]}"
+    printf "%s\n" "$SUDO_PASSWORD" | sudo ${SUDO_ENV_OPT} -S bash ../fix-permissions.sh "${INSTX_PREFIX}"
+else
+    "${MAKE}" "${MAKE_FLAGS[@]}"
+    bash ../fix-permissions.sh "${INSTX_PREFIX}"
+fi
+
+###############################################################################
+
+echo ""
+echo "*****************************************************************************"
+echo "Please run Bash's 'hash -r' to update program cache in the current shell"
+echo "*****************************************************************************"
+
+###############################################################################
+
+touch "${INSTX_PKG_CACHE}/${PKG_NAME}"
+
+cd "${CURR_DIR}" || exit 1
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true;
+then
+    ARTIFACTS=("$MAKE_TAR" "$MAKE_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+fi
+
+exit 0
