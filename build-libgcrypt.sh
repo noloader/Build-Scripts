da9e8e4b8a5945e5c786208235e559a05202efaa
Jeffrey Walton <noloader@gmail.com>
2021-01-16 15:40:43 -0500

    Update libgcrypt recipe

--- build-libgcrypt.sh
+++ build-libgcrypt.sh
@@ -99,11 +99,19 @@ echo "*****************************"
 echo "Configuring package"
 echo "*****************************"
 
+if [[ "${INSTX_DEBUG_MAP}" -eq 1 ]]; then
+    gcrypt_cflags="${INSTX_CFLAGS} -fdebug-prefix-map=${PWD}=${INSTX_SRCDIR}/${GCRYPT_DIR}"
+    gcrypt_cxxflags="${INSTX_CXXFLAGS} -fdebug-prefix-map=${PWD}=${INSTX_SRCDIR}/${GCRYPT_DIR}"
+else
+    gcrypt_cflags="${INSTX_CFLAGS}"
+    gcrypt_cxxflags="${INSTX_CXXFLAGS}"
+fi
+
     PKG_CONFIG_PATH="${INSTX_PKGCONFIG}" \
     CPPFLAGS="${INSTX_CPPFLAGS}" \
     ASFLAGS="${INSTX_ASFLAGS}" \
-    CFLAGS="${INSTX_CFLAGS}" \
-    CXXFLAGS="${INSTX_CXXFLAGS}" \
+    CFLAGS="${gcrypt_cflags}" \
+    CXXFLAGS="${gcrypt_cxxflags}" \
     LDFLAGS="${INSTX_LDFLAGS}" \
     LIBS="${INSTX_LDLIBS}" \
 ./configure \
@@ -181,9 +189,11 @@ MAKE_FLAGS=("install")
 if [[ -n "$SUDO_PASSWORD" ]]; then
     printf "%s\n" "$SUDO_PASSWORD" | sudo ${SUDO_ENV_OPT} -S "${MAKE}" "${MAKE_FLAGS[@]}"
     printf "%s\n" "$SUDO_PASSWORD" | sudo ${SUDO_ENV_OPT} -S bash ../fix-permissions.sh "${INSTX_PREFIX}"
+    printf "%s\n" "$SUDO_PASSWORD" | sudo ${SUDO_ENV_OPT} -S bash ../copy-sources.sh "${PWD}" "${INSTX_SRCDIR}/${GCRYPT_DIR}"
 else
     "${MAKE}" "${MAKE_FLAGS[@]}"
     bash ../fix-permissions.sh "${INSTX_PREFIX}"
+    bash ../copy-sources.sh "${PWD}" "${INSTX_SRCDIR}/${GCRYPT_DIR}"
 fi
 
 ###############################################################################
