f64f5dd41efc78dc8bbc6e27356da9fee3549ce9
Jeffrey Walton <noloader@gmail.com>
2021-01-16 15:45:03 -0500

    Update libtasn1 recipe

--- build-tasn1.sh
+++ build-tasn1.sh
@@ -76,11 +76,19 @@ echo "**********************"
 echo "Configuring package"
 echo "**********************"
 
+if [[ "${INSTX_DEBUG_MAP}" -eq 1 ]]; then
+    tasn1_cflags="${INSTX_CFLAGS} -fdebug-prefix-map=${PWD}=${INSTX_SRCDIR}/${TASN1_DIR}"
+    tasn1_cxxflags="${INSTX_CXXFLAGS} -fdebug-prefix-map=${PWD}=${INSTX_SRCDIR}/${TASN1_DIR}"
+else
+    tasn1_cflags="${INSTX_CFLAGS}"
+    tasn1_cxxflags="${INSTX_CXXFLAGS}"
+fi
+
     PKG_CONFIG_PATH="${INSTX_PKGCONFIG}" \
     CPPFLAGS="${INSTX_CPPFLAGS}" \
     ASFLAGS="${INSTX_ASFLAGS}" \
-    CFLAGS="${INSTX_CFLAGS}" \
-    CXXFLAGS="${INSTX_CXXFLAGS}" \
+    CFLAGS="${tasn1_cflags}" \
+    CXXFLAGS="${tasn1_cxxflags}" \
     LDFLAGS="${INSTX_LDFLAGS}" \
     LIBS="${INSTX_LDLIBS}" \
 ./configure \
@@ -136,9 +144,11 @@ MAKE_FLAGS=("install")
 if [[ -n "$SUDO_PASSWORD" ]]; then
     printf "%s\n" "$SUDO_PASSWORD" | sudo ${SUDO_ENV_OPT} -S "${MAKE}" "${MAKE_FLAGS[@]}"
     printf "%s\n" "$SUDO_PASSWORD" | sudo ${SUDO_ENV_OPT} -S bash ../fix-permissions.sh "${INSTX_PREFIX}"
+    printf "%s\n" "$SUDO_PASSWORD" | sudo ${SUDO_ENV_OPT} -S bash ../copy-sources.sh "${PWD}" "${INSTX_SRCDIR}/${TASN1_DIR}"
 else
     "${MAKE}" "${MAKE_FLAGS[@]}"
     bash ../fix-permissions.sh "${INSTX_PREFIX}"
+    bash ../copy-sources.sh "${PWD}" "${INSTX_SRCDIR}/${TASN1_DIR}"
 fi
 
 ###############################################################################
