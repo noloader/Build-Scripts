40c9cd9001957f75b1152858fa28117288bd9466
Jeffrey Walton <noloader@gmail.com>
2021-03-27 04:17:22 -0400

    Add Mailutils recipe

---
+++ build-mailutils.sh
@@ -0,0 +1,187 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds Mailutils from sources.
+
+MAILUTILS_VER=3.12
+MAILUTILS_TAR=mailutils-${MAILUTILS_VER}.tar.gz
+MAILUTILS_DIR=mailutils-${MAILUTILS_VER}
+PKG_NAME=mailutils
+
+###############################################################################
+
+# Get the environment as needed.
+if [[ "${SETUP_ENVIRON_DONE}" != "yes" ]]; then
+    if ! source ./setup-environ.sh
+    then
+        echo "Failed to set environment"
+        exit 1
+    fi
+fi
+
+# The password should die when this subshell goes out of scope
+if [[ "${SUDO_PASSWORD_DONE}" != "yes" ]]; then
+    if ! source ./setup-password.sh
+    then
+        echo "Failed to process password"
+        exit 1
+    fi
+fi
+
+###############################################################################
+
+if ! ./build-cacert.sh
+then
+    echo "Failed to install CA Certs"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-base.sh
+then
+    echo "Failed to build GNU base packages"
+    exit 1
+fi
+
+###############################################################################
+
+echo ""
+echo "========================================"
+echo "============ GNU Mailutils ============="
+echo "========================================"
+
+echo ""
+echo "**********************"
+echo "Downloading package"
+echo "**********************"
+
+echo ""
+echo "Mailutils ${MAILUTILS_VER}..."
+
+if ! "$WGET" -q -O "$MAILUTILS_TAR" --ca-certificate="$LETS_ENCRYPT_ROOT" \
+     "https://ftp.gnu.org/gnu/mailutils/$MAILUTILS_TAR"
+then
+    echo "Failed to download Mailutils"
+    exit 1
+fi
+
+rm -rf "$MAILUTILS_DIR" &>/dev/null
+gzip -d < "$MAILUTILS_TAR" | tar xf -
+cd "$MAILUTILS_DIR" || exit 1
+
+# Fix sys_lib_dlsearch_path_spec
+bash ../fix-configure.sh
+
+echo ""
+echo "**********************"
+echo "Configuring package"
+echo "**********************"
+
+    PKG_CONFIG_PATH="${INSTX_PKGCONFIG}" \
+    CPPFLAGS="${INSTX_CPPFLAGS}" \
+    ASFLAGS="${INSTX_ASFLAGS}" \
+    CFLAGS="${INSTX_CFLAGS}" \
+    CXXFLAGS="${INSTX_CXXFLAGS}" \
+    LDFLAGS="${INSTX_LDFLAGS}" \
+    LIBS="${INSTX_LDLIBS}" \
+./configure \
+    --build="${AUTOCONF_BUILD}" \
+    --prefix="${INSTX_PREFIX}" \
+    --libdir="${INSTX_LIBDIR}" \
+    --enable-static \
+    --enable-shared \
+    --enable-ipv6 \
+    --disable-radius \
+    --disable-python
+
+if [[ "$?" -ne 0 ]]; then
+    echo ""
+    echo "*****************************"
+    echo "Failed to configure Mailutils"
+    echo "*****************************"
+
+    bash ../collect-logs.sh "${PKG_NAME}"
+    exit 1
+fi
+
+# Escape dollar sign for $ORIGIN in makefiles. Required so
+# $ORIGIN works in both configure tests and makefiles.
+bash ../fix-makefiles.sh
+
+echo ""
+echo "**********************"
+echo "Building package"
+echo "**********************"
+
+MAKE_FLAGS=("-j" "${INSTX_JOBS}" "V=1")
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo ""
+    echo "*****************************"
+    echo "Failed to build Mailutils"
+    echo "*****************************"
+
+    bash ../collect-logs.sh "${PKG_NAME}"
+    exit 1
+fi
+
+# Fix flags in *.pc files
+bash ../fix-pkgconfig.sh
+
+echo ""
+echo "**********************"
+echo "Testing package"
+echo "**********************"
+
+MAKE_FLAGS=("check" "-k" "V=1")
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo ""
+    echo "*****************************"
+    echo "Failed to test Mailutils"
+    echo "*****************************"
+
+    bash ../collect-logs.sh "${PKG_NAME}"
+    exit 1
+fi
+
+echo ""
+echo "**********************"
+echo "Installing package"
+echo "**********************"
+
+MAKE_FLAGS=("install")
+if [[ -n "$SUDO_PASSWORD" ]]; then
+    printf "%s\n" "$SUDO_PASSWORD" | sudo ${SUDO_ENV_OPT} -S "${MAKE}" "${MAKE_FLAGS[@]}"
+    printf "%s\n" "$SUDO_PASSWORD" | sudo ${SUDO_ENV_OPT} -S bash ../fix-permissions.sh "${INSTX_PREFIX}"
+else
+    "${MAKE}" "${MAKE_FLAGS[@]}"
+    bash ../fix-permissions.sh "${INSTX_PREFIX}"
+fi
+
+###############################################################################
+
+echo ""
+echo "*****************************************************************************"
+echo "Please run Bash's 'hash -r' to update program cache in the current shell"
+echo "*****************************************************************************"
+
+###############################################################################
+
+touch "${INSTX_PKG_CACHE}/${PKG_NAME}"
+
+cd "${CURR_DIR}" || exit 1
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true;
+then
+    ARTIFACTS=("$MAILUTILS_TAR" "$MAILUTILS_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+fi
+
+exit 0
