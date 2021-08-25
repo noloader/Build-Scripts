11d85b59be186a7c24ba99a5e460f6974a71693c
Jeffrey Walton <noloader@gmail.com>
2021-08-25 02:31:13 -0400

    Fix OS X version detection

--- setup-environ.sh
+++ setup-environ.sh
@@ -205,14 +205,18 @@ export PATH
 ###############################################################################
 
 # OS X flavors. OS X 10.5 was last to support PowerPC.
-OSX_VERSION=$(system_profiler SPSoftwareDataType 2>&1 | ${GREP} 'System Version:' | ${AWK} '{print $6}')
+# Old OS X uses 'System Version: Mac OS X 10.5.8'.
+# New OS X uses 'System Version: OS X 10.9.5'
+# Hence, we delete the word "Mac" to normalize the stream.
+OSX_VERSION=$(system_profiler SPSoftwareDataType 2>&1 | ${GREP} 'System Version:' | ${SED} 's/Mac //' | ${AWK} '{print $5}')
 OSX_10p10_OR_ABOVE=$(${EGREP} -i -c -E "10\.1[0-9]|1[1-9]\.|[2-9][0-9]" <<< "$OSX_VERSION")
 OSX_10p8_OR_ABOVE=$(${EGREP} -i -c -E "10\.[8-9]|10\.1[0-9]|1[1-9]\.|[2-9][0-9]" <<< "$OSX_VERSION")
-OSX_10p9_OR_BELOW=$(${EGREP} -i -c -E "10\.[0-9]" <<< "$OSX_VERSION")
-OSX_10p5_OR_BELOW=$(${EGREP} -i -c -E "10\.[0-5]" <<< "$OSX_VERSION")
+OSX_10p9_OR_BELOW=$(${EGREP} -i -c -E "10\.[0-9]\." <<< "$OSX_VERSION")
+OSX_10p5_OR_BELOW=$(${EGREP} -i -c -E "10\.[0-5]\." <<< "$OSX_VERSION")
 OSX_10p5_OR_10p6=$(${EGREP} -i -c -E "10\.5|10\.6" <<< "$OSX_VERSION")
 
-export OSX_10p5_OR_BELOW OSX_10p5_OR_10p6 OSX_10p8_OR_ABOVE OSX_10p10_OR_ABOVE
+export OSX_10p5_OR_BELOW OSX_10p5_OR_10p6 OSX_10p9_OR_BELOW
+export OSX_10p8_OR_ABOVE OSX_10p10_OR_ABOVE
 
 if [[ "${OSX_10p5_OR_10p6}" -eq 1 ]]; then
     export MACOSX_DEPLOYMENT_TARGET="10.5"
