0228b4587e1c2e1894033b6efe73bf2eb379ec5f
Jeffrey Walton <noloader@gmail.com>
2021-09-26 12:26:38 -0400

    Bump OpenSSH version

--- build-openssh.sh
+++ build-openssh.sh
@@ -4,8 +4,8 @@
 # This script builds SSH and its dependencies from sources.
 # Also see https://superuser.com/q/961349/173513.
 
-OPENSSH_TAR=openssh-8.7p1.tar.gz
-OPENSSH_DIR=openssh-8.7p1
+OPENSSH_TAR=openssh-8.8p1.tar.gz
+OPENSSH_DIR=openssh-8.8p1
 PKG_NAME=openssh
 
 ###############################################################################
