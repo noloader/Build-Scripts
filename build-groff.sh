b406e498229af44452a6d2100ce095a152590167
Jeffrey Walton <noloader@gmail.com>
2022-01-17 18:41:24 -0500

    Add Groff recipe

---
+++ build-groff.sh
@@ -0,0 +1,195 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds Groff from sources.
+
+GROFF_VER=1.22.3
+GROFF_TAR=groff-${GROFF_VER}.tar.gz
+GROFF_DIR=groff-${GROFF_VER}
+PKG_NAME=groff
+
+###############################################################################
+
+# Get the environment as needed.
+if [[ "${SETUP_ENVIRON_DONE}" != "yes" ]]; then
+    if ! source ./setup-environ.sh
+    then
+        echo "Failed to set environment"
+        exit 1
+    fi
+fi
+
+# The password should die when this subshell goes out of scope
+if [[ "${SUDO_PASSWORD_DONE}" != "yes" ]]; then
+    if ! source ./setup-password.sh
+    then
+        echo "Failed to process password"
+        exit 1
+    fi
+fi
+
+###############################################################################
+
+if ! ./build-cacert.sh
+then
+    echo "Failed to install CA Certs"
+    exit 1
+fi
+
+###############################################################################
+
+echo ""
+echo "========================================"
+echo "================= Groff ================"
+echo "========================================"
+
+echo ""
+echo "**********************"
+echo "Downloading package"
+echo "**********************"
+
+echo ""
+echo "Groff ${GROFF_VER}..."
+
+if ! "$WGET" -q -O "$GROFF_TAR" --ca-certificate="$LETS_ENCRYPT_ROOT" \
+     "https://ftp.gnu.org/gnu/groff/$GROFF_TAR"
+then
+    echo "Failed to download Groff"
+    exit 1
+fi
+
+rm -rf "$GROFF_DIR" &>/dev/null
+gzip -d < "$GROFF_TAR" | tar xf -
+cd "$GROFF_DIR" || exit 1
+
+# Patches are created with 'diff -u' from the pkg root directory.
+if [[ -e ../patch/groff.patch ]]; then
+    echo ""
+    echo "**********************"
+    echo "Patching package"
+    echo "**********************"
+
+    patch -u -p0 < ../patch/groff.patch
+fi
+
+# Fix sys_lib_dlsearch_path_spec
+bash ../fix-configure.sh
+
+echo ""
+echo "**********************"
+echo "Configuring package"
+echo "**********************"
+
+if [[ "${INSTX_DEBUG_MAP}" -eq 1 ]]; then
+    groff_cflags="${INSTX_CFLAGS} -fdebug-prefix-map=${PWD}=${INSTX_SRCDIR}/${GREP_DIR}"
+    groff_cxxflags="${INSTX_CXXFLAGS} -fdebug-prefix-map=${PWD}=${INSTX_SRCDIR}/${GREP_DIR}"
+else
+    groff_cflags="${INSTX_CFLAGS}"
+    groff_cxxflags="${INSTX_CXXFLAGS}"
+fi
+
+    PKG_CONFIG_PATH="${INSTX_PKGCONFIG}" \
+    CPPFLAGS="${INSTX_CPPFLAGS}" \
+    ASFLAGS="${INSTX_ASFLAGS}" \
+    CFLAGS="${groff_cflags}" \
+    CXXFLAGS="${groff_cxxflags}" \
+    LDFLAGS="${INSTX_LDFLAGS}" \
+    LDLIBS="${INSTX_LDLIBS}" \
+    LIBS="${INSTX_LDLIBS}" \
+./configure \
+    --build="${AUTOCONF_BUILD}" \
+    --prefix="${INSTX_PREFIX}" \
+    --libdir="${INSTX_LIBDIR}"
+
+if [[ "$?" -ne 0 ]]; then
+    echo ""
+    echo "**********************"
+    echo "Failed to configure Groff"
+    echo "**********************"
+
+    bash ../collect-logs.sh "${PKG_NAME}"
+    exit 1
+fi
+
+# Escape dollar sign for $ORIGIN in makefiles. Required so
+# $ORIGIN works in both configure tests and makefiles.
+bash ../fix-makefiles.sh
+
+echo ""
+echo "**********************"
+echo "Building package"
+echo "**********************"
+
+MAKE_FLAGS=("MAKEINFO=true" "-j" "${INSTX_JOBS}" "V=1")
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo ""
+    echo "**********************"
+    echo "Failed to build Groff"
+    echo "**********************"
+
+    bash ../collect-logs.sh "${PKG_NAME}"
+    exit 1
+fi
+
+# Fix flags in *.pc files
+bash ../fix-pkgconfig.sh
+
+echo ""
+echo "**********************"
+echo "Testing package"
+echo "**********************"
+
+MAKE_FLAGS=("check" "-k" "V=1")
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo ""
+    echo "**********************"
+    echo "Failed to test Groff"
+    echo "**********************"
+
+    bash ../collect-logs.sh "${PKG_NAME}"
+    exit 1
+fi
+
+echo ""
+echo "**********************"
+echo "Installing package"
+echo "**********************"
+
+MAKE_FLAGS=("install")
+if [[ -n "$SUDO_PASSWORD" ]]; then
+    printf "%s\n" "$SUDO_PASSWORD" | sudo ${SUDO_ENV_OPT} -S "${MAKE}" "${MAKE_FLAGS[@]}"
+    printf "%s\n" "$SUDO_PASSWORD" | sudo ${SUDO_ENV_OPT} -S bash ../fix-permissions.sh "${INSTX_PREFIX}"
+    printf "%s\n" "$SUDO_PASSWORD" | sudo ${SUDO_ENV_OPT} -S bash ../copy-sources.sh "${PWD}" "${INSTX_SRCDIR}/${GREP_DIR}"
+else
+    "${MAKE}" "${MAKE_FLAGS[@]}"
+    bash ../fix-permissions.sh "${INSTX_PREFIX}"
+    bash ../copy-sources.sh "${PWD}" "${INSTX_SRCDIR}/${GREP_DIR}"
+fi
+
+###############################################################################
+
+echo ""
+echo "*****************************************************************************"
+echo "Please run Bash's 'hash -r' to update program cache in the current shell"
+echo "*****************************************************************************"
+
+###############################################################################
+
+touch "${INSTX_PKG_CACHE}/${PKG_NAME}"
+
+cd "${CURR_DIR}" || exit 1
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true;
+then
+    ARTIFACTS=("$GROFF_TAR" "$GROFF_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+fi
+
+exit 0
