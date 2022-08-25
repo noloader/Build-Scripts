55e4bc41ebc534b36e5de309ec8ac4f48fe8ab98
Jeffrey Walton <noloader@gmail.com>
2022-08-25 09:45:57 -0400

    Bump libxml2 version

--- build-libxml2.sh
+++ build-libxml2.sh
@@ -3,8 +3,8 @@
 # Written and placed in public domain by Jeffrey Walton
 # This script builds libxml2 from sources.
 
-# libxml2-2.10.0 is out, but it is missing from the ftp site
-# XML2_VER=2.10.0
+# libxml2-2.10.1 is out, but it is missing from the ftp site
+# XML2_VER=2.10.1
 XML2_VER=2.9.12
 XML2_TAR=libxml2-${XML2_VER}.tar.gz
 XML2_DIR=libxml2-${XML2_VER}
